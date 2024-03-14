2471566442add3d9b8f091c5f3a7e078e58f458d
mik-pat <160538326+mik-pat@users.noreply.github.com>
2024-03-14 13:12:29 -0500

    Add files via upload
    Beginning PS6

---
+++ Ps6_code.R
@@ -0,0 +1,30 @@
+# Problem Set Six Here We Goooooo
+library(tidyverse)
+library(car)
+library(emmeans)
+library(ggplot2)
+library(psych)
+library(agricolae)
+
+# Question 1 Zinc Plating
+
+
+# Question 2 Randomization
+# Assumes s=1, randomizes into 5 blocks
+# Part 1
+
+# Used psych library
+block_rand <- block.random(n=20, c(trt=4))
+headTail(block_rand)
+
+
+
+# Using Agricolae
+
+# Part 2
+# In result DF, blocks 1 & 2 make up the first run, blocks 3 & 4 make up the second
+trt_2 = c("1","2","3","4","5","6")
+block_rand_2 <- design.rcbd(trt_2, 4, serie=0, seed=0)
+block_rand_2
+
+
