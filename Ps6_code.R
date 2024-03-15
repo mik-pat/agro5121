aee3b90f51fd74d7a7fa58e536b90b97a61fc381
mik-pat <160538326+mik-pat@users.noreply.github.com>
2024-03-15 09:48:04 -0500

    Update Ps6_code.R
    Added table export for q2, built dataframe for q3

--- Ps6_code.R
+++ Ps6_code.R
@@ -17,6 +17,8 @@ library(xtable)
 # Used psych library
 block_rand <- block.random(n=20, c(trt=4))
 headTail(block_rand)
+export_table1 <- xtable(block_rand$sketch, auto=TRUE)
+print(export_table1, type="latex")
 
 
 
@@ -29,3 +31,12 @@ block_rand_2 <- design.rcbd(trt_2, 4, serie=0, seed=0)
 block_rand_2
 export_table <- xtable(block_rand_2$sketch, auto=TRUE)
 print(export_table, type="latex")
+
+# Part 3 Respiratory Exchange
+
+resp_ex <- data.frame(Protocol = c(1,2,3,4,5,6,7,8,9),
+                      Subject_1= c(.79, .84, .84, .83, .84, .83, .77, .83, .81),
+                      Subject_2= c(.80, .84, .93, .85, .78, .75, .76, .85, .77),
+                      Subject_3= c(.83, .81, .88, .79, .88, .86, .71, .78, .72))
+resp_ex$Protocol <- as.factor(resp_ex$Protocol)
+summary(resp_ex)
