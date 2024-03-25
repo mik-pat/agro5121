a419d2efa99e36a346f243273290a423920746b1
mik-pat <patri327@umn.edu>
2024-03-25 13:58:40 -0500

    added some code for Q3

--- Ps_7.r
+++ Ps_7.r
@@ -29,3 +29,25 @@ xtable(designII$design, caption = "Design II Randomization", digits = c(0,0,0,0)
 # Design III
 designIII <- ibd(v=8, b = 8, k = 3) # Doesn't satisfy regs for BIBD?
 xtable(designIII$design, caption = "Randomization of Design 3", digits = c(0,0,0,0))
+
+# Q3 Balanced Incomplete Block Design
+# A - theoretical
+# B -  theoretical
+# C - all possible combinations of treatments from 7 with r = 15, calculate number of blocks
+designI <- bibd(v = 7, r = 15, k = 5, lambda = 1) # Requires a B to be specified, doesn't work
+
+# Define parameters
+v <- 7  # Number of treatments
+k <- 5  # Block size (treatments per block)
+r <- 15  # Replications (total observations per treatment)
+
+# Calculate lambda prime (necessary condition for BIBD)
+lambda_prime <- (v - 1) * (k - 1) / (r - k)
+
+# Check if lambda_prime is an integer (necessary condition for BIBD)
+if (lambda_prime %% 1 != 0) {
+  print("A Balanced Incomplete Block Design is not possible with these parameters.")
+} else {
+  b <- (v * (r - k)) / (k * lambda_prime)  # Number of blocks (if BIBD were possible)
+  print("Number of blocks (b) for BIBD:", b)
+} # lambda isn't an integer :/
