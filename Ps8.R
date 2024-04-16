dfecb9cf080d16f1cfb034399588f417e538a2df
mik-pat <160538326+mik-pat@users.noreply.github.com>
2024-04-16 15:17:42 -0500

    Update Ps8.R
    Added code for un-damaged plot.

--- Ps8.R
+++ Ps8.R
@@ -23,10 +23,15 @@ head(df)
 df$Treatment <- as.factor(df$Treatment)
 df$trt <- as.factor(df$trt)
 df$rep <- as.factor(df$rep)
-
+df$undamaged <- 100-df$damage
 # Plot
 ggplot(df) +
   aes(x = Treatment, y = damage, fill = Treatment) +
+  geom_boxplot() +
+  scale_fill_hue(direction = 1) +
+  theme_minimal()
+ggplot(df) +
+  aes(x = Treatment, y = undamaged, fill = Treatment) +
   geom_boxplot() +
   scale_fill_hue(direction = 1) +
   theme_minimal()
@@ -40,16 +45,24 @@ summary(model)
 qqnorm(resid(model))
 qqline(resid(model))
 
+# Redo for undamaged
+umodel <- lmer(undamaged ~ Treatment + (1|rep), data = df)
+plot(umodel)
+summary(umodel)
+# xtable(model)
+# QQ Plot
+qqnorm(resid(umodel))
+qqline(resid(umodel))
 # I think these are all still for error, not sure how to do for random effect
 # Residuals?
 df$Fitercept <- predict(model)
-
+df$FiterceptU <- predict(umodel)
 # Run the Mixed model
-texreg(model)
+texreg(umodel)
 
 # Means separation
 xt <- emmeans(model, list(pairwise~Treatment), adjust = "tukey")
-
+xtu <- emmeans(umodel, list(pairwise~Treatment), adjust = "tukey")
 # Table output not working. Really don't want to do it manually.
 xtable(xt$`emmeans of Treatment`, caption = 'Means Comparison for Treatments', label = NULL, align = NULL,
        digits = 4, display = NULL, auto = FALSE)
@@ -65,10 +78,17 @@ model_means_cld <- cld(object = xt,
                        adjust = "Tukey",
                        Letters = letters,
                        alpha = 0.05)
+umodel_means_cld <- cld(object = xtu,
+                        adjust = "Tukey",
+                        Letters = letters,
+                        alpha = 0.05)
 summary(model_means_cld)
+summary(umodel_means_cld)
 model_means_cld$Treatment <- reorder(model_means_cld$Treatment, desc(model_means_cld$emmean))
+umodel_means_cld$Treatment <- reorder(umodel_means_cld$Treatment, desc(model_means_cld$emmean))
 summary(model_means_cld)
 model_means_cld$Signif <- c("C", "BC", "ABC", "ABC", "AB", "AB", "A")
+umodel_means_cld$Signif <- c("C", "BC", "BC", "ABC", "ABC", "AB", "A")
 summary(model_means_cld)
 # 
 ggplot(model_means_cld) +
@@ -81,10 +101,22 @@ ggplot(model_means_cld) +
 geom_errorbar(aes(ymin = emmean - SE, ymax = emmean + SE), width = 0.2) +  # Adjust width as needed
 
 # Add comparison letters
-
-geom_signif(comparisons = list(c("A", "AB", "ABC", "ABC", "BC", "BC", "C")), map_signif_level = TRUE, textsize = 4, vjust = -0.5) +
+labs(y="Percent damage") +
 geom_text(aes(label = Signif), position = position_dodge(width = 0.9), vjust = -5, size = 3.5)
 # Add comparison letters
 
-# Display the plot
-print(barplot_with_letters)
+
+ggplot(umodel_means_cld) +
+  aes(x = Treatment, y = emmean, fill = Treatment) +
+  geom_col() +
+  scale_fill_hue(direction = 1) +
+  theme_minimal() +
+  # Add error bars
+  
+  geom_errorbar(aes(ymin = emmean - SE, ymax = emmean + SE), width = 0.2) +  # Adjust width as needed
+  labs(y="Percent Undamaged Berries") +
+  # Add comparison letters
+  
+  geom_text(aes(label = Signif), position = position_dodge(width = 0.9), vjust = -2, size = 3.5)
+# Add comparison letters
+
